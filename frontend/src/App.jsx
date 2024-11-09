08cbcff29682213cc29eca49bb42d3f96acd3184
Franklin Hurtado Salazar <137368806+Frankhs899@users.noreply.github.com>
2024-11-09 01:36:21 -0500

    Color palette and font implementation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import './app.css';
+
 function App() {
-  return <div className='bg-yellow-400'>App</div>;
+  return <div className='bg-primary-400'>App</div>;
 }
 
 export default App;
